c525193ab9b679d9c4189595cc6923f92ca2616e
minesamaro <22.maria.amaro@gmail.com>
2023-10-24 14:11:12 +0100

    Created Database Tables

---
+++ src/database/database.sql
@@ -0,0 +1,274 @@
+PRAGMA foreign_keys = ON;
+
+.headers on
+.mode column
+
+-- DROPS
+
+-- Create the Person table
+CREATE TABLE Person (
+    id INT PRIMARY KEY,
+    username TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    birth_date DATE,
+    gender VARCHAR(10)
+);
+
+-- Ensure uniqueness of usernames and emails
+CREATE UNIQUE INDEX idx_unique_username ON Person (username);
+CREATE UNIQUE INDEX idx_unique_email ON Person (email);
+
+-- Create the Message table
+CREATE TABLE Message (
+    id INT PRIMARY KEY,
+    sending_date DATE NOT NULL,
+    TEXT_content TEXT NOT NULL,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    FOREIGN KEY (sender_id) REFERENCES Person(id),
+    FOREIGN KEY (receiver_id) REFERENCES Person(id)
+);
+
+-- Create the Chef and Nutritionist tables with references to Person
+CREATE TABLE Chef (
+    id INT PRIMARY KEY,
+    FOREIGN KEY (id) REFERENCES Person(id)
+);
+
+CREATE TABLE Nutritionist (
+    id INT PRIMARY KEY,
+    FOREIGN KEY (id) REFERENCES Person(id)
+);
+
+-- Create the Formation table
+CREATE TABLE Formation (
+    course_name TEXT,
+    school_name TEXT,
+    academic_level TEXT NOT NULL,
+    graduation_date DATE NOT NULL
+);
+
+-- Create the ChefFormation and NutritionistFormation tables with references
+CREATE TABLE ChefFormation (
+    chef_id INT,
+    course_name TEXT,
+    school_name TEXT,
+    FOREIGN KEY (chef_id) REFERENCES Chef(id),
+    PRIMARY KEY (chef_id, course_name, school_name)
+);
+
+CREATE TABLE NutritionistFormation (
+    nutritionist_id INT,
+    course_name TEXT,
+    school_name TEXT,
+    FOREIGN KEY (nutritionist_id) REFERENCES Nutritionist(id),
+    PRIMARY KEY (nutritionist_id, course_name, school_name)
+);
+
+-- Create the CommonUser table
+CREATE TABLE CommonUser (
+    id INT,
+    height DECIMAL(5, 2),
+    current_weight DECIMAL(5, 2),
+    ideal_weight DECIMAL(5, 2),
+    FOREIGN KEY (id) REFERENCES Person(id),
+    CHECK (height > 0 AND height < 3),
+    CHECK (current_weight > 0 AND current_weight < 600),
+    CHECK (ideal_weight > 0 AND ideal_weight < 600)
+);
+
+-- Create the WeeklyPlan table
+CREATE TABLE WeeklyPlan (
+    id INT PRIMARY KEY,
+    creation_date DATE,
+    total_kcal DECIMAL(7, 2), -- Total calories as a decimal
+    nutritionist_id INT,
+    common_user_id INT,
+    FOREIGN KEY (nutritionist_id) REFERENCES Nutritionist(id),
+    FOREIGN KEY (common_user_id) REFERENCES CommonUser(id)
+);
+
+-- Create the HealthGoal table
+CREATE TABLE HealthGoal (
+    name TEXT PRIMARY KEY
+);
+
+-- Create the UserHealthGoal table
+CREATE TABLE UserHealthGoal (
+    user INT,
+    health_goal TEXT,
+    FOREIGN KEY (user) REFERENCES CommonUser(id),
+    FOREIGN KEY (health_goal) REFERENCES HealthGoal(name),
+    PRIMARY KEY (user, health_goal)
+);
+
+-- Create the AllergyIntolerance table
+CREATE TABLE AllergyIntolerance (
+    name TEXT PRIMARY KEY
+);
+
+-- Create the UserAllergy table
+CREATE TABLE UserAllergy (
+    allergy TEXT,
+    user INT,
+    FOREIGN KEY (allergy) REFERENCES AllergyIntolerance(name),
+    FOREIGN KEY (user) REFERENCES CommonUser(id),
+    PRIMARY KEY (allergy, user)
+);
+
+-- Create the DietaryPreference table
+CREATE TABLE DietaryPreference (
+    name TEXT PRIMARY KEY
+);
+
+-- Create the UserDietPreference table
+CREATE TABLE UserDietPreference (
+    pref TEXT,
+    user INT,
+    FOREIGN KEY (pref) REFERENCES DietaryPreference(name),
+    FOREIGN KEY (user) REFERENCES CommonUser(id),
+    PRIMARY KEY (pref, user)
+);
+
+-- Create the Recipe table
+CREATE TABLE Recipe (
+    id INT PRIMARY KEY,
+    name TEXT NOT NULL,
+    preparation_time INT NOT NULL,
+    difficulty INT NOT NULL,
+    number_of_servings INT NOT NULL,
+    image TEXT NOT NULL,
+    preparation_method TEXT NOT NULL,
+    submission_date DATE NOT NULL,
+    energy DECIMAL(7, 2),  -- Total energy as a decimal
+    carbohydrates DECIMAL(7, 2),  -- Total carbohydrates as a decimal
+    protein DECIMAL(7, 2),  -- Total protein as a decimal
+    fat DECIMAL(7, 2),  -- Total fat as a decimal
+    chef INT NOT NULL,
+    FOREIGN KEY (chef) REFERENCES Chef(id),
+    CHECK (preparation_time > 0),
+    CHECK (number_of_servings > 0),
+    CHECK (difficulty >= 0 AND difficulty <= 5)
+);
+
+-- Create the NutritionistApproval table
+CREATE TABLE NutritionistApproval (
+    nutritionist_id INT,
+    recipe_id INT,
+    approval_date DATE NOT NULL,
+    FOREIGN KEY (nutritionist_id) REFERENCES Nutritionist(id),
+    FOREIGN KEY (recipe_id) REFERENCES Recipe(id)
+);
+
+-- Create the RecipeRanking table
+CREATE TABLE RecipeRanking (
+    user_id INT,
+    recipe_id INT,
+    ranking_date DATE NOT NULL,
+    ranking_value INT NOT NULL,
+    comment TEXT,
+    FOREIGN KEY (user_id) REFERENCES CommonUser(id),
+    FOREIGN KEY (recipe_id) REFERENCES Recipe(id),
+    CHECK (ranking_value >= 0 AND ranking_value <= 5)
+);
+
+-- Create the CookingTechnique table
+CREATE TABLE CookingTechnique (
+    name TEXT PRIMARY KEY,
+    difficulty INT NOT NULL,
+    method_description TEXT NOT NULL,
+    CHECK (difficulty >= 0 AND difficulty <= 5)
+);
+
+-- Create the RecipeCookingTechnique table
+CREATE TABLE RecipeCookingTechnique (
+    recipe_id INT,
+    cooking_technique TEXT,
+    FOREIGN KEY (recipe_id) REFERENCES Recipe(id),
+    FOREIGN KEY (cooking_technique) REFERENCES CookingTechnique(name),
+    PRIMARY KEY (recipe_id, cooking_technique)
+);
+
+-- Create the FoodCategory table
+CREATE TABLE FoodCategory (
+    name TEXT PRIMARY KEY
+);
+
+-- Create the RecipeCategory table
+CREATE TABLE RecipeCategory (
+    category TEXT,
+    recipe_id INT,
+    FOREIGN KEY (category) REFERENCES FoodCategory(name),
+    FOREIGN KEY (recipe_id) REFERENCES Recipe(id),
+    PRIMARY KEY (category, recipe_id)
+);
+
+-- Create the RecipeDietaryPref table
+CREATE TABLE RecipeDietaryPref (
+    dietary_pref TEXT,
+    recipe_id INT,
+    FOREIGN KEY (dietary_pref) REFERENCES DietaryPreference(name),
+    FOREIGN KEY (recipe_id) REFERENCES Recipe(id),
+    PRIMARY KEY (dietary_pref, recipe_id)
+);
+
+-- Create the Ingredient table
+CREATE TABLE Ingredient (
+    id INT PRIMARY KEY,
+    name TEXT NOT NULL,
+    UNIQUE (name)
+);
+
+-- Create the Macronutrient table
+CREATE TABLE Macronutrient (
+    name TEXT PRIMARY KEY,
+    kcal_per_gram DECIMAL(5, 2) NOT NULL,
+    CHECK (kcal_per_gram > 0)
+);
+
+-- Create the IngredientMacronutrient table
+CREATE TABLE IngredientMacronutrient (
+    ingredient_id INT,
+    macronutrient TEXT,
+    quantity_g DECIMAL(7, 2) NOT NULL,
+    FOREIGN KEY (ingredient_id) REFERENCES Ingredient(id),
+    FOREIGN KEY (macronutrient) REFERENCES Macronutrient(name),
+    PRIMARY KEY (ingredient_id, macronutrient)
+);
+
+-- Create the IngredientRecipe table
+CREATE TABLE IngredientRecipe (
+    ingredient_id INT,
+    recipe_id INT,
+    quantity DECIMAL(7, 2) NOT NULL,
+    measurement_unit TEXT NOT NULL,
+    FOREIGN KEY (ingredient_id) REFERENCES Ingredient(id),
+    FOREIGN KEY (recipe_id) REFERENCES Recipe(id),
+    CHECK (quantity > 0)
+);
+
+-- Create the IngredientAllergyIntolerance table
+CREATE TABLE IngredientAllergyIntolerance (
+    ingredient_id INT,
+    allergy_intolerance TEXT,
+    FOREIGN KEY (ingredient_id) REFERENCES Ingredient(id),
+    FOREIGN KEY (allergy_intolerance) REFERENCES AllergyIntolerance(name),
+    PRIMARY KEY (ingredient_id, allergy_intolerance)
+);
+
+-- Create the PlanRecipe table
+CREATE TABLE PlanRecipe (
+    plan_id INT,
+    recipe_id INT,
+    day_week TEXT,
+    portion DECIMAL(5, 2) NOT NULL,
+    time_meal TEXT,
+    FOREIGN KEY (plan_id) REFERENCES WeeklyPlan(id),
+    FOREIGN KEY (recipe_id) REFERENCES Recipe(id),
+    CHECK (day_week IN ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')),
+    CHECK (portion > 0),
+    CHECK (time_meal IN ('Breakfast', 'Lunch', 'Dinner', 'Morning Snack', 'Afternoon Snack', 'Supper'))
+);
