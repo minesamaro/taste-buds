288c0348fa6c4373e4415b319fdf279d0906b063
catarinasem <catarina.s.e.moreira@gmail.com>
2023-12-07 02:14:53 +0000

    commit

---
+++ src/pages/profileUser.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Profile</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/layout.css">
+        <link rel="icon" href="../img/favicon.icon">
+        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+        <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    </head>
+    <body>
+        <header>
+            <div class="header__logo">
+                <a href="../" id="title">Taste Buds</a>
+            </div>
+            <div class="header__nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+            <div id="login-signup">
+            <a href="#login">Log in</a>
+            <!-- <a href="#logout">Log out</a> -->
+            <a href="#signin">Sign in</a>
+            </div>
+        </header>
+        <section id="profile">
+            <h1><a href="#">My profile</a></h1>
+            <div>
+                <p>Username</p>
+                <p>andreiaaaa</p>
+                <p>Name</p>
+                <p>Andreia</p>
+                <p>Surname</p>
+                <p>Silva</p>
+                <p>mail</p>
+                <p>andreia@gmail.com</p>
+                <p>Birth date</p>
+                <p>10/07/2000</p>
+                <p>gender</p>
+                <p>female</p>
+                <p>height</p>
+                <p>1.65</p>
+                <p>weight</p>
+                <p>78</p>
+                <p>ideal weight</p>
+                <p>70</p>
+            </div>
+
+        </section>
+        <section id="img">
+            <img src="profile.png" alt="">
+            <a href="#changePassword">Change Password</a>
+
+
+        </section>
+
+
+    </body>
+
+</html>
