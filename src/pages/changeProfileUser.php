0c4a146d457b12a32535c176bd0c20cfa35457cd
catarinasem <catarina.s.e.moreira@gmail.com>
2023-12-14 01:34:29 +0000

    Change profile page

--- src/pages/changeProfileUser.php
+++ src/pages/changeProfileUser.php
@@ -1,76 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Profile</title>
-        <link rel="stylesheet" href="../css/style.css">
-        <link rel="stylesheet" href="../css/layout.css">
-        <link rel="icon" href="../img/favicon.icon">
-        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-        <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    </head>
-    <body>
-        <header>
-            <div class="header__logo">
-                <a href="../" id="title">Taste Buds</a>
-            </div>
-            <div class="header__nav">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Menu</a></li>
-                    <li><a href="#">About</a></li>
-                </ul>
-            </div>
-            <div id="login-signup">
-            <a href="#login">Log in</a>
-            <!-- <a href="#logout">Log out</a> -->
-            <a href="#signin">Sign in</a>
-            </div>
-        </header>
-        <section id="profile">
-            <h1><a href="#">My profile</a></h1>
-            <form>
-                <label>
-                    Username <input type="text" name="username">
-                </label>
-                <label>
-                    Name <input type="text" name="name">
-                </label>
-                <label>
-                    Surname <input type="text" name="surname">
-                </label>
-                <label>
-                    Mail <input type="text" name="mail">
-                </label>
-                <label>
-                    Birth date <input type="text" name="birthdate">
-                </label>
-                <label>
-                    Gender <input type="text" name="gender">
-                </label>
-                <label>
-                    Height <input type="text" name="height">
-                </label>
-                <label>
-                    Weight <input type="text" name="weight">
-                </label>
-                <label>
-                    Ideal weight <input type="text" name="idealweight">
-                </label>
-                <input type="submit" value="Change Profile">
-                
-            </form>
+<?php
+  require_once(__DIR__ . '/../views/footer.php');
+  require_once(__DIR__ . '/../views/changeProfile.php');
+  require_once(__DIR__ . '/../views/header.php');
 
-        </section>
-        <section id="img">
-            <img src="profile.png" alt="">
-            <a href="#changePassword">Change Password</a>
+ //TODO Associate plan with nutri
 
-
-        </section>
-
-
-    </body>
-
-</html>
+  head("Profile");
+  changeProfile();
+  footer();
+?>
---
+++ src/views/changeProfile.php
@@ -0,0 +1,100 @@
+<?php 
+require_once(__DIR__ . '/../database/person.class.php');
+require_once(__DIR__ . '/../database/commonUser.class.php');
+require_once(__DIR__ . '/../database/chef.class.php');
+require_once(__DIR__ . '/../database/nutritionist.class.php');
+
+function changeProfile(){ 
+    $user_id =2;
+    $personUser = Person::getPersonById($user_id);
+    ?>
+    <article class="content" id="profile">
+        <div class="profile-photo">
+            <img src="profile.png" alt="Profle Picture" width=100px height=100px>
+            <a href="#changePassword">Change Password</a>
+        </div>
+        <div class="profile-info">
+            <h2>My Profile</h2>
+            <div class="username">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="name">
+                <label for="name">First name:</label>
+                <input type="text" id="name" name="name" required>
+            </div>
+            <div class="surname">
+                <label for="surname">Surname:</label>
+                <input type="text" id="surname" name="surname" required>
+            </div>
+
+            <?php
+            //var_dump(Person::isCommonUser($user_id));
+            //var_dump($user_id);
+            //var_dump(Person::isChef($user_id));
+            //var_dump(Person::isNutritionist($user_id));
+            // if user is common_user show info, if user is chef or nutri, show formation
+            if (Person::isCommonUser($user_id)){
+                $commonUser = CommonUser::getCommonUserById($user_id);?>
+                <div class="height">
+                    <label for="height">Height:</label>
+                    <input type="number" id="height" name="height" required>
+                </div> 
+                <div class="curr_weight">
+                    <label for="curr_weight">Current weight:</label>
+                    <input type="number" id="curr_weight" name="curr_weight" required>
+                </div> 
+                <div class="ideal_weight">
+                    <label for="ideal_weight">Ideal Weight:</label>
+                    <input type="number" id="ideal_weight" name="ideal_weight" required>
+                </div> 
+            <?php
+                //
+            } elseif(Person::isChef($user_id)){
+                //
+                $values= Chef::getChefFormation($user_id);
+                ?>
+                <div class="course">
+                    <label for="course">Course name:</label>
+                    <input type="text" id="course" name="course" required>
+                </div> 
+                <div class="school">
+                    <label for="school">School name:</label>
+                    <input type="text" id="school" name="school" required>
+                </div> <div class="Level">
+                    <label for="academic_lvl">Academic level:</label>
+                    <input type="text" id="academic_lvl" name="academic_lvl" required>
+                </div> <div class="date">
+                    <label for="date">Graduation date:</label>
+                    <input type="date" id="date" name="date" required>
+                </div> 
+                
+            <?php } elseif( Person::isNutritionist($user_id)){
+                $values= Nutritionist::getNutriFormation($user_id);
+                ?>
+                <div class="Course">
+                    <p>Course:</p>
+                    <p> <?php echo $values[1] ?></p>
+                </div> 
+                <div class="School">
+                    <p>School:</p>
+                    <p> <?php echo $values[2] ?></p>
+                </div> <div class="Level">
+                    <p>Academic Level:</p>
+                    <p> <?php echo $values[3] ?></p>
+                </div> <div class="date">
+                    <p>Graduation Date:</p>
+                    <p> <?php echo $values[0] ?></p>
+                </div> 
+                
+            <?php 
+
+            }
+
+
+            ?>
+        </div>
+    </article>
+<?php
+}
+?>
