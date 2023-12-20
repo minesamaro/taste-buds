a428dc74f10695416d81785d01f41eb4121a6043
JoanaCaleiro <joana.caleiro@gmail.com>
2023-12-20 21:35:22 +0000

    nome actions

--- src/views/messageForm.php
+++ src/views/messageForm.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/../database/message.class.php');
 function messageForm($selectedPersonId) { ?>
 
  <!-- Form to write and send a message to the selected person -->
- <form id="message-write_message" action="../actions/action_sendMessage.php" method="post">
+ <form id="message-write_message" action="../actions/actionSendMessage.php" method="post">
 
 <input type="hidden" name="receiver_id" value="<?php echo $selectedPersonId; ?>">
 <label for="message_content">Write a message:</label>
