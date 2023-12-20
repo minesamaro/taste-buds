7dd9609ddfac77b8870151f288c27730d6d4e43b
JoanaCaleiro <joana.caleiro@gmail.com>
2023-12-20 21:23:58 +0000

    pprofile ratings

--- src/views/profileRatings.php
+++ src/views/profileRatings.php
@@ -41,22 +41,34 @@ function profileRatings()
             
             $recipe=Recipe::getRecipeById($recipeId);
         ?> 
-            <section class="card">
-                <div class="card-header">
-                    <h4>
-                        <a href="../pages/recipe.php?recipe_id=<?php echo $recipeId?>"><? echo $recipe->name ?></a>
-
-                    </h4>
-                </div>
-                    
-                    <h6><?php echo $rating->ratingValue ?></h6>
-                    <h6><?php echo $rating->ratingDate ?></h6>
-                    <h6><?php echo $rating->comment ?></h6>
-        
-                </div>
-
-
-            </section>
+            <div class="card">
+                        
+                    <div class="rating-text">
+                        <div class="rating-top">
+                            <a id="rating-name" href="../pages/recipe.php?recipe_id=<?php echo $recipeId; ?>"><? echo $recipe->name; ?></a>
+                            <div class="rating-stars">
+                                <?php for ($i = 0; $i < $rating->ratingValue; $i++) { ?>
+                                    <span class="">&#x2605</span>
+                                <?php } 
+                                for ($i = 0; $i < 5 - $rating->ratingValue; $i++) { ?>
+                                    <span class="">&#x2606</span>
+                                <?php } ?>
+                            </div>
+                        </div>
+                        <div class="rating-bottom">
+                            <div>
+                            <img class="rating-profile_photo" src="<?php echo $user->profile_photo; ?>" alt="<?php echo $user->username; ?>'s profile photo">
+                            <span class="recipe-rating_username"> <? echo $user->username; ?> </span>
+                            </div>
+                            <div>
+                            <span class="recipe-rating_date"> <? echo date("d-m-Y", strtotime($rating->ratingDate)); ?> </span>
+                            </div>
+                        </div>
+                        <div class="rating-comment">
+                            <span class="recipe-rating_comment"> <? echo $rating->comment; ?> </span>
+                        </div>
+                    </div>
+            </div>
         <?php } 
         ?> 
         </article>
