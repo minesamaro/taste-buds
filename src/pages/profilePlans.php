96bdfc84885cbe585ebddf2a0b29eda1bcdf5220
JoanaCaleiro <joana.caleiro@gmail.com>
2023-12-20 21:00:42 +0000

    profile corrections

--- src/pages/profilePlans.php
+++ src/pages/profilePlans.php
@@ -5,8 +5,8 @@
   require_once(__DIR__ . '/../database/weeklyPlan.class.php');
   require_once(__DIR__ . '/../database/commonUser.class.php');
 
-//   $id = $_GET['id'];
-//   // Check if user is logged in, and the plan corresponds to the user either as nutritionist or as common user
+   $id = $_GET['id'];
+  // Check if user is logged in, and the plan corresponds to the user either as nutritionist or as common user
 //   if (!isset($_SESSION['user_id']) || !WeeklyPlan::checkPlanUser(intval($id), $_SESSION['user_id'])) {
 //          header("Location: ../pages/404.php");
 //      }
--- src/pages/profileRatings.php
+++ src/pages/profileRatings.php
@@ -4,9 +4,12 @@
   require_once(__DIR__ . '/../views/profileRatings.php');
 
 
+if (!isset($_SESSION['user_id'])) {
+  header("Location: ../pages/404.php");
 
-
+} else {
   head("Ratings"); 
   profileRatings();
   footer();
+}
 ?>
