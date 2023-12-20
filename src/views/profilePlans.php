b5d12e6daabd79295c9236b31fa7b7c8cf0531a6
catarinasem <catarina.s.e.moreira@gmail.com>
2023-12-20 13:25:34 +0000

    profile done

--- src/views/profilePlans.php
+++ src/views/profilePlans.php
@@ -11,10 +11,15 @@ function profilePlans()
     $user_id=$_SESSION['user_id'] ;
     if (Person::isNutritionist($user_id)){
         $plans=Nutritionist::getPlansByNutriId($user_id); 
+        $isNutri=true;
     }
     elseif(Person::isCommonUser($user_id)){
         $plans=CommonUser::getPlansByUserId($user_id); 
+        $isNutri=false;
+        
     }
+    
+
 ?>
     
     <article class="content">
@@ -29,9 +34,19 @@ function profilePlans()
         ?> 
             <section class="card">
                 <div class="card-header">
-                    <h4>
-                        <a href="../pages/plan.php?id=<?php echo $plan->id ?>">Weekly Plan <?php echo $plan->id ?></a>
-                    </h4>
+                    <?php if($isNutri){
+                        $common=Person::getPersonById($plan->idCommonUser);
+                        ?><h4>
+                            <a href="../pages/plan.php?id=<?php echo $plan->id ?>">Weekly Plan for <?php echo $common->first_name ?> <?php echo $common->surname ?></a>
+                        </h4> <?php
+                    }else{
+                    $nutri=Person::getPersonById($plan->idNutritionist);
+                        ?>
+                            <h4>
+                               <a href="../pages/plan.php?id=<?php echo $plan->id ?>">Weekly Plan by <?php echo $nutri->first_name ?> <?php echo $nutri->surname ?></a>
+                            </h4> <?php
+                        }?>
+                    
                         
                     <h6><?php 
                     $date = new DateTime($plan->creationDate);
